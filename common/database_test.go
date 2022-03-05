f12857207da80e83e186b88329095a30ef028694
Miftah Salam <salam.miftah@gmai.com>
2022-03-05 12:08:56 +0700

    database test first implementation

---
+++ common/database_test.go
@@ -0,0 +1,18 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDatabaseConnection(t *testing.T) {
+	asserts := assert.New(t)
+	db := Init()
+
+	LogI.Println("masuk")
+
+	sqlDB, err := db.DB()
+	asserts.NoError(err, "Db should be available")
+	asserts.NoError(sqlDB.Ping(), "Db should be connected")
+}
