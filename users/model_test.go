80f68c1ba6cc45a8a1089ed7448c80955d123842
Miftah Salam <salam.miftah@gmail.com>
2022-03-08 06:33:50 +0700

    test complited

--- users/model_test.go
+++ users/model_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
 var image_url = "https://golang.org/doc/gopher/frontpage.png"
 var userModelMock UserModel = UserModel{
 	ID:           2,
@@ -21,4 +23,17 @@ func TestUserModel(t *testing.T) {
 
 	err := userModelMock.checkPassword("")
 	asserts.Error(err, "empty password should return error")
+
+	err = userModelMock.setPassword("")
+	asserts.Error(err, "empty password cannot be set null")
+
+	err = userModelMock.setPassword("123456")
+	asserts.NoError(err, "password should be set successfully")
+	asserts.NotEqual(userModelMock.PasswordHash, "123456", "hashed password should not equal to password")
+
+	err = userModelMock.checkPassword("12345")
+	asserts.Error(err, "password should be checked and not valid")
+
+	err = userModelMock.checkPassword("123456")
+	asserts.NoError(err, "password should be checked and valid")
 }
