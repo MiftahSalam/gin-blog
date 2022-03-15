074e89d89341ea3069718381a4d8e10d187ae798
Miftah Salam <salam.miftah@gmail.com>
2022-03-15 16:55:08 +0700

    first run implementation

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-	// "log"
-
 	"github.com/MiftahSalam/gin-blog/common"
+	"github.com/MiftahSalam/gin-blog/users"
+	userMiddlewares "github.com/MiftahSalam/gin-blog/users/middlewares"
+	"github.com/MiftahSalam/gin-blog/users/models"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
+func Migrate() {
+	models.AuthoMigrate()
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		common.LogE.Println("Error loading file .env")
 	}
-	common.LogI.Println("Test log format")
 
-	// users.CheckDotEnv()
+	db := common.Init()
+	Migrate()
+	defer func() {
+		sqlDB, err := db.DB()
+		if err != nil {
+			common.LogE.Fatal("get db instance error: ", err)
+		}
+		sqlDB.Close()
+	}()
+
+	router := gin.Default()
+	v1 := router.Group("/api")
+
+	users.Users(v1.Group("/users"))
+	v1.Use(userMiddlewares.AuthMiddleware(false))
+
+	router.Run()
 }
