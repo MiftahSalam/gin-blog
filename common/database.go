67777a3fc55b6c6e51dc98255c27ffa309784617
Miftah Salam <salam.miftah@gmail.com>
2022-03-15 17:42:16 +0700

    add switch database connection between development and production mode

--- common/database.go
+++ common/database.go
@@ -17,18 +17,25 @@ type Database struct {
 var DB *gorm.DB
 
 func Init() *gorm.DB {
-	dbPort, err := strconv.Atoi(os.Getenv("DATABASE_PORT"))
-	if err != nil {
-		LogE.Fatal("Error while loading DATABASE_PORT. Err: ", err)
+	productionMode := os.Getenv("PRODUCTION")
+	var dsn string
+
+	if productionMode == "" {
+		dbPort, err := strconv.Atoi(os.Getenv("DATABASE_PORT"))
+		if err != nil {
+			LogE.Fatal("Error while loading DATABASE_PORT. Err: ", err)
+		}
+
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta",
+			os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"), dbPort)
+	} else {
+		dsn = os.Getenv("DATABASE_URL")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta",
-		os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"), dbPort)
 	enableLog, err := strconv.Atoi(os.Getenv("DATABASE_LOGGING"))
 	if err != nil {
 		LogE.Fatal("Error while loading DATABASE_LOGGING. Err: ", err)
 	}
-	dsn = os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.LogLevel(enableLog)),
