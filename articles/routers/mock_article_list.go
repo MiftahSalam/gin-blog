7cb493108ff319fab3748cb4374e4569b16ebe7b
Miftah Salam <salam.miftah@gmail.com>
2022-06-01 12:05:25 +0700

    finishing test cases

--- articles/routers/mock_article_list.go
+++ articles/routers/mock_article_list.go
@@ -8,10 +8,9 @@ import (
 	"net/http/httptest"
 
 	"github.com/MiftahSalam/gin-blog/common"
-	// userModels "github.com/MiftahSalam/gin-blog/users/models"
 	userServices "github.com/MiftahSalam/gin-blog/users/services"
 
-	// articleModels "github.com/MiftahSalam/gin-blog/articles/model"
+	articleModels "github.com/MiftahSalam/gin-blog/articles/model"
 	articleServices "github.com/MiftahSalam/gin-blog/articles/services"
 	"github.com/stretchr/testify/assert"
 )
@@ -45,115 +44,217 @@ var MockListArticle = []RouterMockTest{
 			a.Equal(uint(4), uint(len(jsonResp.Articles)))
 			a.Equal(uint(4), jsonResp.ArticleCount)
 
-			// a.Equal(articleModels.ArticlesMock[0].Title, jsonResp.Article.Title)
-			// a.Equal(articleModels.ArticlesMock[0].Body, jsonResp.Article.Body)
-			// a.Equal(articleModels.ArticlesMock[0].Author.UserModel.Username, jsonResp.Article.Author.Username)
+			// a.Equal(articleModels.ArticlesMock[0].Title, jsonResp.Articles[0].Title)
+			// a.Equal(articleModels.ArticlesMock[0].Body, jsonResp.Articles[0].Body)
+			// a.Equal(articleModels.ArticlesMock[0].Author.UserModel.Username, jsonResp.Articles[0].Author.Username)
 		},
 	},
-	/*
-		{
-			UserMockTest: userServices.MockTests{
-				TestName: "no error (another article): Get Article Test",
-				Init: func(req *http.Request) {
-					req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", common.GetToken(userModels.UsersMock[userModels.UserMockNumber-int(userModels.CurrentRecordCount)-1].ID)))
-				},
-				Url:             "/article/my-article1",
-				Method:          "GET",
-				Body:            "",
-				ResponseCode:    http.StatusOK,
-				ResponsePattern: "",
-				Msg:             "valid data and should return StatusOK",
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (all articles limit 2): Get Article List Test",
+			Init: func(req *http.Request) {
 			},
-			ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
-				response_body, _ := ioutil.ReadAll(w.Body)
-
-				common.LogI.Println("response_body", string(response_body))
-
-				var jsonResp articleServices.ArticleResponse
-				err := json.Unmarshal(response_body, &jsonResp)
-				if err != nil {
-					common.LogE.Println("Cannot umarshal json content with error: ", err)
-				}
-				a.NoError(err)
-				common.LogI.Println("jsonResp", jsonResp)
-				a.Equal(articleModels.ArticlesMock[1].Title, jsonResp.Article.Title)
-				a.Equal(articleModels.ArticlesMock[1].Body, jsonResp.Article.Body)
-				a.Equal(articleModels.ArticlesMock[1].Author.UserModel.Username, jsonResp.Article.Author.Username)
-			},
+			Url:             "/article/?limit=2",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
 		},
-		{
-			UserMockTest: userServices.MockTests{
-				TestName: "no error (articles feed): Get Article Test",
-				Init: func(req *http.Request) {
-					req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", common.GetToken(userModels.UsersMock[1].ID)))
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
 
-					articleModels.ArticleUsersModelMock[1].UserModel.Following(articleModels.ArticleUsersModelMock[0].UserModel)
-					articleModels.ArticleUsersModelMock[1].UserModel.Following(articleModels.ArticleUsersModelMock[2].UserModel)
+			// common.LogI.Println("response_body", string(response_body))
 
-				},
-				Url:             "/article/feed",
-				Method:          "GET",
-				Body:            "",
-				ResponseCode:    http.StatusOK,
-				ResponsePattern: "",
-				Msg:             "valid data and should return StatusOK",
-			},
-			ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
-				response_body, _ := ioutil.ReadAll(w.Body)
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
 
-				common.LogI.Println("response_body", string(response_body))
-
-				var jsonResp articleServices.ArticlesResponse
-				err := json.Unmarshal(response_body, &jsonResp)
-				if err != nil {
-					common.LogE.Println("Cannot umarshal json content with error: ", err)
-				}
-				a.NoError(err)
-				common.LogI.Println("jsonResp", jsonResp)
-				a.Equal(uint(2), jsonResp.ArticleCount)
-				a.Equal(uint(2), uint(len(jsonResp.Articles)))
-			},
+			a.Equal(uint(2), uint(len(jsonResp.Articles)))
+			a.Equal(uint(2), jsonResp.ArticleCount)
 		},
-		{
-			UserMockTest: userServices.MockTests{
-				TestName: "error (article not found): Get Article Test",
-				Init: func(req *http.Request) {
-					req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", common.GetToken(userModels.UsersMock[1].ID)))
-				},
-				Url:             "/article/sdsd",
-				Method:          "GET",
-				Body:            "",
-				ResponseCode:    http.StatusNotFound,
-				ResponsePattern: "",
-				Msg:             "valid data and should return StatusNotFound",
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (all articles offset 2): Get Article List Test",
+			Init: func(req *http.Request) {
 			},
-			ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
-				response_body, _ := ioutil.ReadAll(w.Body)
-
-				common.LogI.Println("response_body", string(response_body))
-
-				a.Equal(`{"errors":{"article":"article not found"}}`, string(response_body))
-			},
+			Url:             "/article/?offset=1",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
 		},
-		{
-			UserMockTest: userServices.MockTests{
-				TestName: "error unauthorized (no token present): Get Article Test",
-				Init: func(req *http.Request) {
-				},
-				Url:             "/article/my-article0",
-				Method:          "GET",
-				Body:            "",
-				ResponseCode:    http.StatusUnauthorized,
-				ResponsePattern: "",
-				Msg:             "valid data and should return StatusUnauthorized",
-			},
-			ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
-				response_body, _ := ioutil.ReadAll(w.Body)
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
 
-				common.LogI.Println("response_body", string(response_body))
+			// common.LogI.Println("response_body", string(response_body))
 
-				a.Equal(`{"error":"no token present in request"}`, string(response_body))
-			},
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(3), uint(len(jsonResp.Articles)))
+			a.Equal(uint(3), jsonResp.ArticleCount)
 		},
-	*/
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (list by tag mock): Get Article List Test",
+			Init: func(req *http.Request) {
+			},
+			Url:             "/article/?tag=mock",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
+		},
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
+
+			// common.LogI.Println("response_body", string(response_body))
+
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(1), uint(len(jsonResp.Articles)))
+			a.Equal(uint(1), jsonResp.ArticleCount)
+		},
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (list by author0): Get Article List Test",
+			Init: func(req *http.Request) {
+			},
+			Url:             "/article/?author=user0",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
+		},
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
+
+			// common.LogI.Println("response_body", string(response_body))
+
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(2), uint(len(jsonResp.Articles)))
+			a.Equal(uint(2), jsonResp.ArticleCount)
+		},
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (list by author1): Get Article List Test",
+			Init: func(req *http.Request) {
+			},
+			Url:             "/article/?author=user1",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
+		},
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
+
+			// common.LogI.Println("response_body", string(response_body))
+
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(1), uint(len(jsonResp.Articles)))
+			a.Equal(uint(1), jsonResp.ArticleCount)
+		},
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (list by author11): Get Article List Test",
+			Init: func(req *http.Request) {
+			},
+			Url:             "/article/?author=user11",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
+		},
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
+
+			// common.LogI.Println("response_body", string(response_body))
+
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(0), uint(len(jsonResp.Articles)))
+			a.Equal(uint(0), jsonResp.ArticleCount)
+		},
+	},
+	{
+		UserMockTest: userServices.MockTests{
+			TestName: "no error (list by favorite user1): Get Article List Test",
+			Init: func(req *http.Request) {
+				articleModels.ArticlesMock[0].FavoriteBy(articleModels.ArticleUsersModelMock[1])
+			},
+			Url:             "/article/?favorited=user1",
+			Method:          "GET",
+			Body:            "",
+			ResponseCode:    http.StatusOK,
+			ResponsePattern: "",
+			Msg:             "valid data and should return StatusOK",
+		},
+		ResponseTest: func(w *httptest.ResponseRecorder, a *assert.Assertions) {
+			response_body, _ := ioutil.ReadAll(w.Body)
+
+			common.LogI.Println("response_body", string(response_body))
+
+			var jsonResp articleServices.ArticlesResponse
+			err := json.Unmarshal(response_body, &jsonResp)
+			if err != nil {
+				common.LogE.Println("Cannot umarshal json content with error: ", err)
+			}
+			a.NoError(err)
+			// common.LogI.Println("jsonResp", jsonResp)
+
+			a.Equal(uint(1), uint(len(jsonResp.Articles)))
+			a.Equal(uint(1), jsonResp.ArticleCount)
+			a.Equal(articleModels.ArticleUsersModelMock[0].UserModel.Username, jsonResp.Articles[0].Author.Username)
+
+			articleModels.ArticlesMock[0].UnFavoriteBy(&articleModels.ArticleUsersModelMock[1])
+
+		},
+	},
 }
