921af617a010e7fa53ace316cadc4642a45ea803
Miftah Salam <salam.miftah@gmail.com>
2022-07-16 11:08:35 +0700

    move article retrieve and comment list handle from Articles router to ArticlesAnonymous router

--- articles/routers/router.go
+++ articles/routers/router.go
@@ -7,18 +7,18 @@ import (
 
 func Articles(router *gin.RouterGroup) {
 	router.POST("/", services.ArticleCreate)
-	router.GET("/:slug", services.ArticleRetrieve)
 	router.PUT("/:slug", services.ArticleUpdate)
 	router.POST("/:slug/favorite", services.ArticleFavorite)
 	router.DELETE("/:slug/favorite", services.ArticleUnFavorite)
 	router.DELETE("/:slug", services.ArticleDelete)
 	router.POST("/:slug/comment", services.ArticleCommentCreate)
-	router.GET("/:slug/comments", services.ArticleCommentList)
 	router.DELETE("/:slug/comment/:id", services.ArticleCommentDelete)
 }
 
 func ArticlesAnonymous(router *gin.RouterGroup) {
 	router.GET("/", services.ArticleList)
+	router.GET("/:slug", services.ArticleRetrieve)
+	router.GET("/:slug/comments", services.ArticleCommentList)
 }
 
 func Tags(router *gin.RouterGroup) {
